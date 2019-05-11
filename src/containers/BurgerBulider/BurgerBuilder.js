cf82ce543d65a18b2b0b0288072aab03dbfe763e
dkcvarma <dkchaitanyavarma06@gmail.com>
2019-05-11 14:26:06 +0530

    Preparing the Folder Structure

--- src/containers/BurgerBulider/BurgerBuilder.js
+++ src/containers/BurgerBulider/BurgerBuilder.js
@@ -9,7 +9,7 @@ import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
-import * as actionTypes from '../../store/actions';
+import * as actionTypes from '../../store/actions/actionsTypes';
 
 // const INGREDIENT_PRICES = {
 //     salad: 0.5,
